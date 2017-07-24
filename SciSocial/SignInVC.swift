b34aa8402aada2c46e943e3b97e679772e8f482c
berishaerblin <berishaerblin@gmail.com>
2017-07-24 18:38:59 +0200

    First steps of a socail app made using Firebase. Comleted setting up Firebase integration

---
+++ SciSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  SciSocial
+//
+//  Created by Erblin Berisha on 7/24/17.
+//  Copyright © 2017 Erblin Berisha. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
