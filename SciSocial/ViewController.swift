a2e5c9ba4d184ba21fe69eaf59b36828c99dc1e2
berishaerblin <berishaerblin@gmail.com>
2017-07-24 18:11:41 +0200

    Initial Commit

---
+++ SciSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SciSocial
+//
+//  Created by Erblin Berisha on 7/24/17.
+//  Copyright © 2017 Erblin Berisha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
