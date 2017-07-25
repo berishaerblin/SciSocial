cc6b0ee92e967b99776a380030814787437e7b3a
berishaerblin <berishaerblin@gmail.com>
2017-07-25 17:51:28 +0200

    Sign in ui custom classes complete

---
+++ SciSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SciSocial
+//
+//  Created by Erblin Berisha on 7/25/17.
+//  Copyright © 2017 Erblin Berisha. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
