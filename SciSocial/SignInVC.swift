5ed521ef13622ded8ea3d291bb7d8a4bbd460ec0
berishaerblin <berishaerblin@gmail.com>
2017-07-26 00:22:40 +0200

    Completed email auth

--- SciSocial/SignInVC.swift
+++ SciSocial/SignInVC.swift
@@ -13,6 +13,10 @@ import FacebookLogin
 
 class SignInVC: UIViewController {
 
+    
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var pwdField: FancyField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -49,5 +53,24 @@ class SignInVC: UIViewController {
             }
         }
     }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        if let email = emailField.text, let pwd = pwdField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("Blinnky: Email user authenticated with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("Blinnky: Unable to authenticate with Firebase using email")
+                        } else {
+                            print("Blinnky: Successfully authenticated with Firebase")
+                        }
+                    })
+                }
+            })
+        }
+    }
+    
 }
 
