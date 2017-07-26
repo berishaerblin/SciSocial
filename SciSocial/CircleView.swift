43931c39dd3e52f1b9ac3cf0eea4b8c7318f5a0d
berishaerblin <berishaerblin@gmail.com>
2017-07-26 03:17:21 +0200

    Completed feed ui

--- SciSocial/CircleView.swift
+++ SciSocial/CircleView.swift
@@ -1,24 +1,30 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SciSocial
 //
-//  Created by Erblin Berisha on 7/25/17.
+//  Created by Erblin Berisha on 7/26/17.
 //  Copyright © 2017 Erblin Berisha. All rights reserved.
 //
 
 import UIKit
-@IBDesignable
-class FancyView: UIView {
 
+@IBDesignable
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+        
     }
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width/2
+
+    }
+    
 
 }
